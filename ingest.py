c9b0b26eb0e668f01a3a324d87384ea2b0606da0
Rebecca Bilbro <bilbro@gmail.com>
2016-03-05 07:16:35 -0500

    preliminary implementation of ingest

--- ingest.py
+++ ingest.py
@@ -16,6 +16,7 @@ Ingest all the necessary datasets from UCI that are appropriate for classificati
 # Imports
 #####################################################################
 
+import os
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -34,7 +35,7 @@ BASE_STORE = "../orlo/data/"
 def getPage(url):
     """
     Get all the text and links from the page.
-    We don't want to do this very often, so when we do, save result to a file.
+    We don't want to do very often, so when we do, save result to a file.
     """
     result = requests.get(url)
     text = result.content
@@ -51,20 +52,43 @@ def readPage(fname):
         soup = BeautifulSoup(text)
         for a in soup.find_all('a', href=re.compile("datasets/")):
             pname = re.sub("datasets/","",str(a['href']))
+            # name = pname.split()
             name = re.sub(r'[^\w\s]','',pname).lower()
             if name not in urls:
                 urls[name] = pname
         return urls
+
+def parsePage(key,mltype):
+    """
+    Reads child urls from main url and determines whether the leaf node dataset
+    is for the machine learning type ("Classification", "Regression", "Relational").
+    """
+    results = {}
+    data_desc = requests.get(BASE_URL+"datasets/"+key)
+    desc = data_desc.content
+    soup = BeautifulSoup(desc)
+    if len(soup.find_all(text=re.compile(mltype)))>0:
+        for a in soup.find_all('a', href=re.compile("machine-learning-databases")):
+            data_idx = requests.get((BASE_URL+a['href'].strip("../")))
+            index = data_idx.content
+            soup = BeautifulSoup(index)
+            for b in soup.find_all('a', href=re.compile(r'\b\.xls\b|\b\.data\b|\b\.csv\b')):
+                results[b['href']] = BASE_URL+a['href'].strip("../")+"/"+b['href']
+    return results
+
 
 def getData(surl,fname):
     """
     For a dataset URL, use requests library to grab the dataset,
     and save it to the data directory using the name.
     """
-    response = requests.get(singURL)
-    outpath = os.path.abspath(fname)
-    with open(outpath, 'w') as f:
-        f.write(response.content)
+    try:
+        response = requests.get(surl)
+        outpath = os.path.abspath(fname)
+        with open(outpath, 'w') as f:
+            f.write(response.content)
+    except:
+        print "Couldn't get the file for %d" %surl
 
 def ingest(urldict):
     """
@@ -74,23 +98,42 @@ def ingest(urldict):
         getData(BASE_URL+"datasets/"+val, BASE_STORE+key+".csv")
 
 if __name__ == '__main__':
-    my_urls = readPage("../orlo/data/uci_text.txt")
-    # for key,val in my_urls.items():
-    #     print BASE_URL+val, BASE_STORE+key+".csv"
+    # Create parent url dict
+    links = readPage("../orlo/data/uci_text.txt")
+    for name,pname in links.items():
+        x = parsePage(pname,"Classification")
+        for k,v in x.items():
+            getData(v,BASE_STORE+k)
 
-
-    kinship_desc_url = "https://archive.ics.uci.edu/ml/datasets/kinship"
-    kinship_data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/kinship/"
-    response = requests.get(kinship_desc_url)
-    txt = response.content
-    soup = BeautifulSoup(txt)
-    if len(soup.find_all(text=re.compile('Relational')))>0:
-        response = requests.get(kinship_data_url)
-        txt = response.content
-        soup = BeautifulSoup(txt)
-        for a in soup.find_all('a', href=re.compile(".data")):
-          print a['href']
-
-    # print soup.find_all(text=re.compile('Classification'))
-    # for a in soup.find_all('a', href=re.compile(".data")):
+    # testsets = {"pageblocks":"https://archive.ics.uci.edu/ml/machine-learning-databases/page-blocks/page-blocks.data.Z",
+    #             "winequality":"https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",
+    #             "lenses":"https://archive.ics.uci.edu/ml/machine-learning-databases/lenses/lenses.data"}
+    # for k,v in testsets.items():
+    #
+    # with open('uci_links.csv', 'w') as f:
+    #     f.write(links)
+    # print ("Regression:")
+    # parsePage("cardiotocography","Regression")
+    # parsePage("lungcancer","Regression")
+    # parsePage("annealing","Regression")
+    # parsePage("kinship","Regression")
+    # print ("Classification:")
+    # parsePage("cardiotocography","Classification")
+    # parsePage("lung+cancer","Classification")
+    # parsePage("annealing","Classification")
+    # parsePage("kinship","Classication")
+    # print ("Relational:")
+    # parsePage("cardiotocography","Relational")
+    # parsePage("lungcancer","Relational")
+    # parsePage("annealing","Relational")
+    # parsePage("kinship","Relational")
+    # print ("Clustering:")
+    # parsePage("cardiotocography","Clustering")
+    # parsePage("lungcancer","Clustering")
+    # parsePage("annealing","Clustering")
+    # parsePage("kinship","Clustering")
+    # data_desc = requests.get('https://archive.ics.uci.edu/ml/datasets/annealing')
+    # desc = data_desc.content
+    # soup = BeautifulSoup(desc)
+    # for a in soup.find_all('a', href=re.compile("machine-learning-databases")):
     #     print a['href']
