55e0fd79618e7eda24c1a9b5d89198b75d8a6821
Rebecca Bilbro <bilbro@gmail.com>
2016-03-05 07:33:43 -0500

    classi will apply different classifiers to UCI datasets

---
+++ classi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# classi.py
+
+
+# Title:        Apply Classification to UCI Datasets for ROC Curves Tour
+# Author:       Rebecca Bilbro
+# Date:         3/5/16
+# Organization: District Data Labs
+
+
+"""
+Apply different classification methods to applicable datasets from UCI.
+Use highest-scoring-estimator wins approach.
+"""
+
+#####################################################################
+# Imports
+#####################################################################
+
+#####################################################################
+# Global Variables
+#####################################################################
+
+#####################################################################
+# Classification
+#####################################################################
+
+
+if __name__ == '__main__':
+    main()
